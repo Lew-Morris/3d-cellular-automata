de02226e5f03c8da03200bb90927aca4b99a0bca
Lew-Morris <morrisl2701@gmail.com>
2022-11-16 17:52:12 +0000

    New bevy startup system

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    App::new()
+        .add_startup_system(startup)
+        .run();
+}
+
+fn startup() {
+    println!("Hello world!");
 }
