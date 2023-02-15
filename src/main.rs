12962ea343f62f408c76259369f314c480516123
Lew-Morris <morrisl2701@gmail.com>
2023-02-15 15:21:19 +0000

    Cells are stored as list Bounds now user defined

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::fmt;
-use std::fmt::{Formatter};
+use std::{fmt, io::stdin};
 
 fn main() {
     let my_first_cell = SimpleCell {
@@ -9,6 +8,30 @@ fn main() {
         neighbours: 5,
     };
     println!("Original Cell\n-------------\n{}", my_first_cell);
+    let mut cell_list: Vec<SimpleCell> = Vec::new();
+    println!("Enter bounds size: ");
+
+    // User chooses bounds size
+    let mut input_bounds: String = String::new();
+    stdin()
+        .read_line(&mut input_bounds)
+        .expect("Failed to read input");
+
+    let bounds: i32 = input_bounds.trim().parse::<i32>().expect("Input not a number");
+
+    // Input validation
+    if bounds > 512 { panic!("Bounds are too large!") }
+    else if bounds < 0 { panic!("Bounds cannot be negative!") }
+
+    for z in 0..bounds {
+        for y in 0..bounds {
+            for x in 0..bounds {
+                cell_list.push(SimpleCell::new(x, y, z, 5, 0))
+            }
+        }
+    }
+    let count = cell_list.iter().count();
+    println!("Total cells: {}", count)
 }
 
 #[derive(Clone)]
@@ -20,6 +43,14 @@ struct SimpleCell {
 }
 
 impl SimpleCell {
+    pub fn new(x: i32, y: i32, z: i32, states: i32, neighbours: i32) -> SimpleCell {
+        SimpleCell {
+            position: (x, y, z),
+            alive: true,
+            states,
+            neighbours,
+        }
+    }
     // pub fn get_position(&self) ->(i32, i32, i32) {
     //     self.position
     // }
@@ -36,25 +67,25 @@ impl SimpleCell {
     //     self.neighbours
     // }
 
-    pub fn set_position(&mut self, position: (i32, i32, i32)) {
-        self.position = position;
-    }
-
-    pub fn set_alive(&mut self, alive: bool) {
-        self.alive = alive;
-    }
-
-    pub fn set_states(&mut self, states: i32){
-        self.states = states;
-    }
-
-    pub fn set_neighbours(&mut self, neighbours: i32) {
-        self.neighbours = neighbours;
-    }
+    // pub fn set_position(&mut self, position: (i32, i32, i32)) {
+    //     self.position = position;
+    // }
+    //
+    // pub fn set_alive(&mut self, alive: bool) {
+    //     self.alive = alive;
+    // }
+    //
+    // pub fn set_states(&mut self, states: i32){
+    //     self.states = states;
+    // }
+    //
+    // pub fn set_neighbours(&mut self, neighbours: i32) {
+    //     self.neighbours = neighbours;
+    // }
 }
 
 impl fmt::Display for SimpleCell {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Position: {}, {}, {}\nAlive: {}\nStates: {}\nNeighbours: {}\n",
                self.position.0, self.position.1, self.position.2, self.alive, self.states, self.neighbours)
     }
