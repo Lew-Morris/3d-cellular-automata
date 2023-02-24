8fa72c93ead2398dd6fcaf5b5e7b585c3e55014d
Lew-Morris <morrisl2701@gmail.com>
2023-02-24 13:01:43 +0000

    Implemented neighbourhood

--- src/neighbours.rs
+++ src/neighbours.rs
@@ -2,57 +2,55 @@ use bevy::math::{IVec3};
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Neighbourhood {
-    Moore,
     VonNeumann,
+    Moore,
 }
 
 impl Neighbourhood {
     pub fn get_neighbourhood_iter(&self) -> &'static [IVec3] {
         match self {
+            Neighbourhood::VonNeumann => &VON_NEUMANN[..],
             Neighbourhood::Moore => &MOORE[..],
-            Neighbourhood::VonNeumann => {
-                println!("{:#?}", VON_NEUMANN)
-                &VON_NEUMANN[..]
-            }
         }
     }
 }
 
-// todo! Store these correctly
-// pub static VON_NEUMANN: [IVec3; 6] = [
-//     IVec3::from_array(IVec3(1, 0, 0)),
-//     IVec3::from_array(IVec3(0, 1, 0)),
-//     IVec3::from_array(IVec3(0, 0, 1)),
-//     IVec3::from_array(IVec3(-1, 0, 0)),
-//     IVec3::from_array(IVec3(0, -1, 0)),
-//     IVec3::from_array(IVec3(0, 0, -1)),
-// ];
-//
-// pub static MOORE: [IVec3; 26] = [
-//     IVec3::from_array(IVec3(-1, -1, -1)),
-//     IVec3::from_array(IVec3(0, -1, -1)),
-//     IVec3::from_array(IVec3(1, -1, -1)),
-//     IVec3::from_array(IVec3(-1, 0, -1)),
-//     IVec3::from_array(IVec3(0, 0, -1)),
-//     IVec3::from_array(IVec3(1, 0, -1)),
-//     IVec3::from_array(IVec3(-1, 1, -1)),
-//     IVec3::from_array(IVec3(0, 1, -1)),
-//     IVec3::from_array(IVec3(1, 1, -1)),
-//     IVec3::from_array(IVec3(-1, -1, 0)),
-//     IVec3::from_array(IVec3(0, -1, 0)),
-//     IVec3::from_array(IVec3(1, -1, 0)),
-//     IVec3::from_array(IVec3(-1, 0, 0)),
-//     IVec3::from_array(IVec3(1, 0, 0)),
-//     IVec3::from_array(IVec3(-1, 1, 0)),
-//     IVec3::from_array(IVec3(0, 1, 0)),
-//     IVec3::from_array(IVec3(1, 1, 0)),
-//     IVec3::from_array(IVec3(-1, -1, 1)),
-//     IVec3::from_array(IVec3(0, -1, 1)),
-//     IVec3::from_array(IVec3(1, -1, 1)),
-//     IVec3::from_array(IVec3(-1, 0, 1)),
-//     IVec3::from_array(IVec3(0, 0, 1)),
-//     IVec3::from_array(IVec3(1, 0, 1)),
-//     IVec3::from_array(IVec3(-1, 1, 1)),
-//     IVec3::from_array(IVec3(0, 1, 1)),
-//     IVec3::from_array(IVec3(1, 1, 1)),
-// ];
+// Check VN neighbourhood (Cells touching a face of target cell)
+pub static VON_NEUMANN: [IVec3; 6] = [
+    IVec3::from_array([1, 0, 0]),
+    IVec3::from_array([0, 1, 0]),
+    IVec3::from_array([0, 0, 1]),
+    IVec3::from_array([-1, 0, 0]),
+    IVec3::from_array([0, -1, 0]),
+    IVec3::from_array([0, 0, -1]),
+];
+
+// Check Moore Neighbourhood (All cells around target cell)
+pub static MOORE: [IVec3; 26] = [
+    IVec3::from_array([-1, -1, -1]),
+    IVec3::from_array([0, -1, -1]),
+    IVec3::from_array([1, -1, -1]),
+    IVec3::from_array([-1, 0, -1]),
+    IVec3::from_array([0, 0, -1]),
+    IVec3::from_array([1, 0, -1]),
+    IVec3::from_array([-1, 1, -1]),
+    IVec3::from_array([0, 1, -1]),
+    IVec3::from_array([1, 1, -1]),
+    IVec3::from_array([-1, -1, 0]),
+    IVec3::from_array([0, -1, 0]),
+    IVec3::from_array([1, -1, 0]),
+    IVec3::from_array([-1, 0, 0]),
+    IVec3::from_array([1, 0, 0]),
+    IVec3::from_array([-1, 1, 0]),
+    IVec3::from_array([0, 1, 0]),
+    IVec3::from_array([1, 1, 0]),
+    IVec3::from_array([-1, -1, 1]),
+    IVec3::from_array([0, -1, 1]),
+    IVec3::from_array([1, -1, 1]),
+    IVec3::from_array([-1, 0, 1]),
+    IVec3::from_array([0, 0, 1]),
+    IVec3::from_array([1, 0, 1]),
+    IVec3::from_array([-1, 1, 1]),
+    IVec3::from_array([0, 1, 1]),
+    IVec3::from_array([1, 1, 1]),
+];
