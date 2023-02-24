d0a1481b50abb08f12164784e2e32b6fe9b0dfdd
Lew-Morris <morrisl2701@gmail.com>
2023-02-24 13:00:34 +0000

    Implementing neighbours.rs, added function

--- src/rule.rs
+++ src/rule.rs
@@ -1,9 +1,9 @@
-// use crate::{neighbours::Neighbourhood};
+use crate::{neighbours::Neighbourhood};
 
 #[derive(Clone, Copy)]
 pub struct Rule {
     pub states: u8,
-    // pub neighbourhood: Neighbourhood, todo! Make a list of IVec3
+    pub neighbourhood: Neighbourhood, // todo! Make a list of IVec3
     // pub birth: Value todo! Implement 'Value' struct
 }
 
@@ -11,10 +11,15 @@ impl Rule {
     pub fn new(states: u8) -> Rule {
         Rule {
             states,
+            neighbourhood: Neighbourhood::VonNeumann,
         }
     }
 
     pub fn get_states(self) -> u8 {
         self.states
+    }
+
+    pub fn get_neighbourhood(self) -> Neighbourhood {
+        self.neighbourhood
     }
 }
