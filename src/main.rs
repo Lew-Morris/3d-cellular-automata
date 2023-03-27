41f5c0ce41b2273139b757c38e40f08cca0c00ee
Lew-Morris <morrisl2701@gmail.com>
2023-03-27 12:49:32 +0100

    Implemented bevy app

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,115 @@
-mod rule;
-mod simple_cell;
+// First-Party
+// Modules
+pub mod cell_event;
+mod cell_render;
+mod helper;
 mod neighbours;
-// mod tests;
+mod rotating_camera;
+mod rule;
+mod cells;
+// mod setup;
 
-use std::{io::stdin};
-use crate::neighbours::Neighbourhood;
-use crate::simple_cell::SingleThreaded;
+// Imports
+use cell_render::*;
+use neighbours::Neighbourhood::*;
+use rule::*;
+use cell_event::CellStatesChangedEvent;
+use cells::sims::Example;
+use crate::cells::Sims;
+// use setup::*;
+
+// Third-Party
+// Imports
+use rotating_camera::{RotatingCamera, RotatingCameraPlugin}; //todo! Implement rotating camera
+use bevy::{prelude::*, render::view::NoFrustumCulling};
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
 fn main() {
-    // let states: u8 = 5;
-    // let current_rule: Rule = Rule::new(states); // New rule with default values
+    let mut task_pool_settings = TaskPoolOptions::default();
+    task_pool_settings.async_compute.percent = 1.0f32;
+    task_pool_settings.compute.percent = 0.0f32;
+    task_pool_settings.io.percent = 0.0f32; // always use 1
 
-    // Choose bounds size
-    println!("Enter bounds size [1-512]: ");
-    let mut input_bounds: String = String::new();
-    stdin()
-        .read_line(&mut input_bounds)
-        .expect("Failed to read input");
-
-     let bounds: i32 = input_bounds
-        .trim()
-        .parse::<i32>()
-        .expect("Input not a number");
-
-    // Input validation
-    if bounds > 512 { panic!("Bounds are too large!") }
-    else if bounds < 0 { panic!("Bounds cannot be negative!") }
-    else if bounds == 0 { panic!("Bounds cannot be zero!") }
-
-    // Instantiate SingleThreaded
-    let mut cells: SingleThreaded = SingleThreaded::new();
-    cells.set_bounds(bounds);
-    let cell_count = cells.count_cells(true);
-
-    println!("There are {} live cells", cell_count);
-
-    // A simple loop to test the `is_boundary` method
-    loop {
-        // Request input from user
-        println!("Enter in a cell's position: ");
-        let mut input: String = String::new();
-        stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input");
-
-        // Parse and store input as i32 (32-bit integer)
-        let position: i32 = input
-            .trim()
-            .parse::<i32>()
-            .expect("Input was not a number");
-
-        // Validate input
-        if position - 1 > bounds.pow(3) - 1 { panic!("Position out of bounds - The maximum value was {}", bounds.pow(3)) }
-        else if position - 1 < 0 { panic!("Position out of bounds - too small!") }
-        else {
-            // Output the specified cell's properties (Value/ Boundary)
-            cells.print_cell(position as usize);
-            println!("Boundary Cell: {}", is_boundary(bounds as usize, position as usize))
-        }
-    }
-
+    App::new()
+        .insert_resource(task_pool_settings)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(EguiPlugin)
+        .insert_resource(ClearColor(Color::rgb(0.9f32, 0.9f32, 0.9f32))) // Background color
+        .add_event::<CellStatesChangedEvent>()
+        .add_plugin(RotatingCameraPlugin)
+        .add_plugin(CellMaterialPlugin)
+        .add_plugin(cells::SimsPlugin)
+        .add_startup_system(configure_visuals_system)
+        .add_startup_system( setup)
+        .run();
 }
 
-// Check if the cell is on the boundary using the index
-fn is_boundary(bounds: usize, index: usize) -> bool {
-    // todo! Return the type of boundary
-    let size = bounds.pow(3);
-    match index {
-        // first layer
-        index if index < bounds * bounds => true,
-        // last layer
-        index if index >= size - (bounds * bounds) => true,
-        // first column
-        index if index % bounds == 0 => true,
-        // last column
-        index if index % bounds == bounds - 1 => true,
-        // first slice
-        index if index % (bounds * bounds) < bounds => true,
-        // last slice
-        index if index % (bounds * bounds) >= bounds * (bounds - 1) => true,
-        _ => false,
-    }
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut sims: ResMut<Sims>) {
+    sims.add_sim(
+        "Simple Cell".into(),
+        Box::new(cells::simple_cell::SingleThreaded::new()),
+    );
+
+    sims.add_example(Example {
+        name: "Builder".into(),
+        rule: Rule {
+            survival_rule: Value::new(&[2, 6, 9]),
+            birth: Value::new(&[4, 6, 8, 9, 10]),
+            states: 10,
+            neighbourhood: VonNeumann,
+        },
+        color_method: ColorMethod::DistToCenter,
+        color1: Color::YELLOW,
+        color2: Color::RED,
+    });
+
+    sims.add_example(Example {
+        name: "Pyramid".into(),
+        rule: Rule {
+            survival_rule: Value::from_range(0..=6),
+            birth: Value::new(&[1, 3]),
+            states: 2,
+            neighbourhood: VonNeumann,
+        },
+
+        color_method: ColorMethod::DistToCenter,
+        color1: Color::GREEN,
+        color2: Color::BLUE,
+    });
+
+    sims.set_example(0);
+
+    commands.spawn((
+        meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        // SpatialBundle::INHERITED_IDENTITY,
+        Transform::from_xyz(0.0, 0.0, 15.0),
+        GlobalTransform::default(),
+        InstanceMaterialData (
+            (1..=10)
+                .flat_map(|x| (1..=100).map(move |y| (x as f32 / 10.0, y as f32 / 10.0)))
+                .map(|(x, y)| InstanceData {
+                    position: Vec3::new(x * 10.0 - 5.0, y * 10.0 - 5.0, 0.0),
+                    scale: 1.0,
+                    color: Color::hsla(x * 360., y, 0.5, 1.0).as_rgba_f32(),
+                })
+                .collect(),
+        ),
+        Visibility::Visible,
+        ComputedVisibility::default(),
+        NoFrustumCulling,
+    ));
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    })
+        .insert(RotatingCamera::default());
 }
 
-//
-// impl fmt::Display for Position {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "{}, {}, {}", self.x, self.y, self.z)
-//     }
-// }
-
+fn configure_visuals_system(mut contexts: EguiContexts) {
+    contexts.ctx_mut().set_visuals(egui::Visuals {
+        window_rounding: 0.0.into(),
+        ..Default::default()
+    });
+}
