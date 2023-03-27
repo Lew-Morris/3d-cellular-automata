81a2b353292d3a9f474032274e4327bfb9bd6913
Lew-Morris <morrisl2701@gmail.com>
2023-03-27 12:55:38 +0100

    Added rotating camera

---
+++ src/rotating_camera.rs
@@ -0,0 +1,40 @@
+use bevy::math::{vec3, Quat};
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct RotatingCamera {
+    pub rotation: f32,
+    pub last_tick: f32,
+    pub speed: f32,
+    pub dist: f32,
+    pub center: Vec3,
+}
+
+impl Default for RotatingCamera {
+    fn default() -> Self {
+        Self {
+            rotation: 0f32,
+            last_tick: 0f32,
+            speed: 0.01f32,
+            dist: 150f32,
+            center: vec3(0.0, 0.0, 0.0),
+        }
+    }
+}
+
+pub struct RotatingCameraPlugin;
+impl Plugin for RotatingCameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(update_tick);
+    }
+}
+
+pub fn update_tick(mut cameras: Query<(&mut RotatingCamera, &mut Transform)>) {
+    for (mut camera, mut transform) in cameras.iter_mut() {
+        let delta = 1.0f32;
+        camera.rotation += delta * camera.speed;
+        let rotation = Quat::from_axis_angle(Vec3::Y, camera.rotation);
+        transform.translation = camera.center + (rotation * Vec3::Z * camera.dist);
+        transform.look_at(camera.center, Vec3::Y);
+    }
+}
