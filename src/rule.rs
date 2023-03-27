bfb234719c555c4c0853a74ef430b9e17fdbee3d
Lew-Morris <morrisl2701@gmail.com>
2023-03-27 12:51:25 +0100

    Value struct, cell colouring

--- src/rule.rs
+++ src/rule.rs
@@ -1,25 +1,76 @@
-use crate::{neighbours::Neighbourhood};
+use bevy::prelude::Color;
+use std::ops::RangeInclusive;
+use crate::{helper, neighbours::Neighbourhood};
 
-#[derive(Clone, Copy)]
-pub struct Rule {
-    pub states: u8,
-    pub neighbourhood: Neighbourhood, // todo! Make a list of IVec3
-    // pub birth: Value todo! Implement 'Value' struct
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Value([bool; 27]);
+
+impl Value {
+    pub fn new(indices: &[u8]) -> Self {
+        let mut result = Value([false; 27]);
+        for index in indices {
+            result.0[*index as usize] = true;
+        }
+        result
+    }
+
+    pub fn from_range(indices: RangeInclusive<u8>) -> Self {
+        let mut result = Value([false; 27]);
+        for index in indices {
+            result.0[index as usize] = true;
+        }
+        result
+    }
+
+    #[allow(dead_code)]
+    pub fn in_range(&self, value: u8) -> bool {
+        self.0[value as usize]
+    }
+
+    pub fn in_range_incorrect(&self, value: u8) -> bool {
+        *self.0.get(value as usize).unwrap_or(&false)
+    }
 }
 
-impl Rule {
-    pub fn new(states: u8) -> Rule {
-        Rule {
-            states,
-            neighbourhood: Neighbourhood::VonNeumann,
+#[derive(Clone, Copy, PartialEq)]
+pub struct Rule {
+    pub states: u8,
+    pub neighbourhood: Neighbourhood,
+    pub birth: Value,
+    pub survival_rule: Value,
+}
+
+#[allow(dead_code)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ColorMethod {
+    Single,
+    StateLerp,
+    DistToCenter,
+    Neighbour,
+}
+
+impl ColorMethod {
+    pub fn color(
+        &self,
+        c1: Color,
+        c2: Color,
+        states: u8,
+        state: u8,
+        neighbours: u8,
+        dist_to_center: f32,
+    ) -> Color {
+        match self {
+            ColorMethod::Single => c1,
+            ColorMethod::StateLerp => {
+                let dt = state as f32 / states as f32;
+                helper::lerp_color(c1, c2, dt)
+            }
+            ColorMethod::DistToCenter => helper::lerp_color(c1, c2, dist_to_center),
+            ColorMethod::Neighbour => {
+                let dt = neighbours as f32 / 26f32;
+                helper::lerp_color(c1, c2, dt)
+            }
         }
-    }
-
-    pub fn get_states(self) -> u8 {
-        self.states
-    }
-
-    pub fn get_neighbourhood(self) -> Neighbourhood {
-        self.neighbourhood
     }
 }
