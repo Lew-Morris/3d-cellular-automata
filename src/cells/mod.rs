f25cd47b203aa5de059a41b5bd0ab35ef6f04f99
Lew-Morris <morrisl2701@gmail.com>
2023-03-27 12:54:48 +0100

    Moved file

---
+++ src/cells/mod.rs
@@ -0,0 +1,27 @@
+use crate::cell_render::CellRenderer;
+use crate::rule::Rule;
+use bevy::tasks::TaskPool;
+
+pub trait Sim: Send + Sync {
+    fn update(&mut self, rule: &Rule, task_pool: &TaskPool);
+    fn render(&self, data: &mut CellRenderer);
+
+    fn reset(&mut self) {
+        let bounds = self.bounds();
+        self.set_bounds(0);
+        self.set_bounds(bounds);
+    }
+
+    fn spawn_noise(&mut self, rule: &Rule);
+
+    fn cell_count(&self) -> usize;
+
+    fn bounds(&self) -> i32;
+
+    fn set_bounds(&mut self, new_bounds: i32) -> i32;
+}
+
+pub mod sims;
+pub use sims::*;
+
+pub(crate) mod simple_cell;
