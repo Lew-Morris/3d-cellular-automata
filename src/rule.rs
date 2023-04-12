9839c0e56534443f51e3c546b06c32a36fbc6b8d
Lew-Morris <morrisl2701@gmail.com>
2023-04-12 11:05:44 +0100

    Optimised imports, refactored ColourMethod

--- src/rule.rs
+++ src/rule.rs
@@ -1,7 +1,5 @@
-use crate::{neighbours::Neighbourhood, utilities::state_colour};
-use bevy::prelude::Color;
+use crate::neighbours::Neighbourhood;
 use std::ops::RangeInclusive;
-use ColourMethod::{Colour1, Colour2, DistToCenter, Index, Neighbour, State};
 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Value([bool; 27]);
@@ -47,48 +45,4 @@ pub struct Rule {
     pub survival: Value,
     pub neighbourhood: Neighbourhood,
     pub states: u8,
-}
-
-#[allow(dead_code)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum ColourMethod {
-    Colour1,
-    Colour2,
-    State,
-    DistToCenter,
-    Neighbour,
-    Index,
-}
-
-impl ColourMethod {
-    // Set the colour method
-    pub fn set_colour(
-        &self,
-        c1: Color,
-        c2: Color,
-        current_state: u8,
-        total_states: u8,
-        neighbours: u8,
-        distance_to_centre: f32,
-        index: usize,
-        total_cells: usize,
-    ) -> Color {
-        match self {
-            Colour1 => c1,
-            Colour2 => c2,
-            State => {
-                let gradient = current_state as f32 / total_states as f32;
-                state_colour(c1, c2, gradient)
-            }
-            DistToCenter => state_colour(c1, c2, distance_to_centre),
-            Neighbour => {
-                let gradient = neighbours as f32 / 26f32;
-                state_colour(c1, c2, gradient)
-            }
-            Index => {
-                let gradient = index as f32 / total_cells as f32;
-                state_colour(c1, c2, gradient)
-            }
-        }
-    }
 }
